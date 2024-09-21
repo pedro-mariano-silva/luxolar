356cd22650110ebe7a1a6be06b61f4d0ff489ce0
unknown <pedro_mariano.silva@outlook.com>
2024-09-21 15:05:44 -0300

    adicionando o botão 'Menu' com responsividade.

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,17 +1,27 @@
+import{useState} from 'react'
 import {Link} from 'react-router-dom'
 
 //ASSETS
 import './Header.css'
 import Logo from '../../assets/logo.svg'
 
+//COMPONENTS
+import Button from '../Button/Button'
+
 function Header (){
+    const[isOpen, setIsOpen]=useState(false)
     return(
         <header>
             <div className="container">
                 <div className="al-center d-flex jc-space-between">
                     <Link to="/"> <img src={Logo}/> </Link>
-                    <nav>
-                    <ul className='d-flex'>
+                    <div className="mobile-menu">
+                        <Button buttonStyle="secondary" onClick={()=>setIsOpen(!isOpen)}>Menu</Button>              
+
+                    </div>
+                    <nav className={`${isOpen ? 'open' : '' }`}>
+                        <Button buttonStyle="unstyled" className="mobile-menu close-btn" onClick={()=>setIsOpen(!isOpen)}>X</Button>
+                    <ul className="d-flex">
                         <li>
                             <Link to='/'>Home</Link>
                             <Link to='/about'>About</Link>
@@ -22,9 +32,12 @@ function Header (){
                          
                     </ul>
                 </nav>
+
+                    
                 </div>
                
             </div>
+            
         </header>
     )
 
